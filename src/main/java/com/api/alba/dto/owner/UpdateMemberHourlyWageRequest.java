67ea13f29f95e62545f0f06588d061576ec5fbde
eeesnghyun <szhyun2002@gmail.com>
2026-05-04 23:56:44 +0900

    refactor: remove unnecessary @NotNull annotation from UpdateMemberHourlyWageRequest

--- src/main/java/com/api/alba/dto/owner/UpdateMemberHourlyWageRequest.java
+++ src/main/java/com/api/alba/dto/owner/UpdateMemberHourlyWageRequest.java
@@ -3,12 +3,10 @@ package com.api.alba.dto.owner;
 import lombok.Getter;
 
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
 public class UpdateMemberHourlyWageRequest {
-    @NotNull
     @DecimalMin(value = "0.0", inclusive = false)
     private BigDecimal hourlyWage;
 }
